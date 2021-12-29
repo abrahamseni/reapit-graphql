7c9e7b3633801a5d2d4ce9f23350ffb45f2d49d5
abrahamseni <aagung@reapit.com>
2021-12-29 08:23:35 +0700

    chore: delete comments

--- src/platform-api/request.ts
+++ src/platform-api/request.ts
@@ -1,21 +1,4 @@
 import { GraphQLClient } from 'graphql-request'
-// import { BASE_HEADERS } from '../constants/api'
-// import { reapitConnectBrowserSession } from '../core/connect-session'
-
-// function graphqlRequestClient() {
-//   let headers = {}
-//   reapitConnectBrowserSession.connectSession().then((session) => {
-//     if (!session) return
-//     headers = {
-//       ...BASE_HEADERS,
-//       authorization: session.idToken,
-//       'reapit-connect-token': session.accessToken,
-//     }
-//   })
-//   return new GraphQLClient('https://graphql.reapit.cloud/graphql', {
-//     headers,
-//   })
-// }
 
 const graphqlRequestClient = new GraphQLClient(
   'https://graphql.reapit.cloud/graphql'
